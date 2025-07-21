c08c78b4edf5193d32c8d97d58a7912c2e1ca40c
Danny Lee <danny.lee.tech@protonmail.com>
2025-07-20 23:12:42 -0400

    Add Config files

---
+++ internal/config/config.go
@@ -0,0 +1,6 @@
+package config
+
+type Config struct {
+	GeminiApiKey     string `yaml:"gemini_api_key"`
+	PushBulletApiKey string `yaml:"pushbullet_api_key"`
+}
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"github.com/danny-lee-tech/book-subscription-alert/internal/config"
 	"github.com/danny-lee-tech/book-subscription-alert/internal/fairyloot"
 	"github.com/danny-lee-tech/book-subscription-alert/internal/history"
 	"github.com/danny-lee-tech/book-subscription-alert/internal/illumicrate"
@@ -14,30 +16,32 @@ import (
 	"github.com/danny-lee-tech/book-subscription-alert/internal/owlcrate"
 	"github.com/danny-lee-tech/book-subscription-alert/internal/summarizer"
 	"google.golang.org/genai"
+	"gopkg.in/yaml.v2"
 )
 
+var DefaultConfigLocation = "configs/config.yml"
+
 func main() {
-	geminiApiKey := os.Args[1]
-	pushBulletApiKey := os.Args[2]
+	config, err := getConfig()
+	if err != nil {
+		panic(err)
+	}
 
 	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
 	client, err := genai.NewClient(ctx, &genai.ClientConfig{
-		APIKey:  geminiApiKey,
+		APIKey:  config.GeminiApiKey,
 		Backend: genai.BackendGeminiAPI,
 	})
 	if err != nil {
 		panic(err)
 	}
 
-	var notif *notifier.Notifier
-	if pushBulletApiKey != "" {
-		notif = &notifier.Notifier{
-			PushBullet: &notifier.PushBulleter{
-				APIKey: pushBulletApiKey,
-				Tag:    "book-subscription-alert",
-				Title:  "New Book Subscription Alert",
-			},
-		}
+	notif := &notifier.Notifier{
+		PushBullet: &notifier.PushBulleter{
+			APIKey: config.PushBulletApiKey,
+			Tag:    "book-subscription-alert",
+			Title:  "New Book Subscription Alert",
+		},
 	}
 
 	err = checkOwlCrate(client, notif)
@@ -144,5 +148,46 @@ func checkIllumicrate(client *genai.Client, notif *notifier.Notifier) error {
 	}
 
 	illumicrateHistory.RecordItemIfNotExist(postUrl)
+	return nil
+}
+
+func getConfig() (config.Config, error) {
+	configLocation := getConfigLocation()
+	cfgBytes, err := os.ReadFile(configLocation)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	var cfg config.Config
+	err = yaml.Unmarshal(cfgBytes, &cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	err = validateConfig(&cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func getConfigLocation() string {
+	configLocation := os.Getenv("CONFIG_LOCATION")
+	if configLocation != "" {
+		return configLocation
+	}
+	return DefaultConfigLocation
+}
+
+func validateConfig(cfg *config.Config) error {
+	if cfg.GeminiApiKey == "" {
+		return errors.New("missing required field: gemini_api_key")
+	}
+
+	if cfg.PushBulletApiKey == "" {
+		return errors.New("missing required field: pushbullet_api_key")
+	}
+
 	return nil
 }
